76e00bc5dc75aefbb252c74006382fff9575f5da
Shrey Rohatgi <rohatgishrey@gmail.com>
2019-09-03 12:42:01 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -12,3 +12,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'        
